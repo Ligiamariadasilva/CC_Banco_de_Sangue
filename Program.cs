2ffebbbc141cce538418044a1dc4bcc787702de8
LuizXlenda <riquestorrer@gmail.com>
2025-05-17 01:17:56 -0300

    Integração com SQLite e aprimoramento nas rotas e arquivo Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using DoadoresApi.Data;
+using DoadoresApi.Models;
 using DoadoresApi.Routes;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("DoadoresDB"));
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=doadores.db"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,10 +16,50 @@ app.UseSwaggerUI();
 
 app.MapGet("/", () => "API de Doação de Sangue");
 
-// Registrar rotas separadas
 RotaGET.Map(app);
 RotaPOST.Map(app);
 RotaPUT.Map(app);
 RotaDELETE.Map(app);
 
+DoacoesBancoDeDados(app);
+
 app.Run();
+
+
+void DoacoesBancoDeDados(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    context.Database.Migrate();
+
+    if (!context.Doadores.Any())
+    {
+        var doadoresIniciais = new List<Doador>
+        {
+            new() { Id = 1, Nome = "Luiz Storrer", TipoSanguineo = "O+", DataUltimaDoacao = new DateTime(2024, 5, 10) },
+            new() { Id = 2, Nome = "Amanda Edling", TipoSanguineo = "A-", DataUltimaDoacao = new DateTime(2024, 3, 22) },
+            new() { Id = 3, Nome = "Wellington", TipoSanguineo = "B+", DataUltimaDoacao = new DateTime(2023, 12, 5) },
+            new() { Id = 4, Nome = "Ligia", TipoSanguineo = "O-", DataUltimaDoacao = new DateTime(2023, 12, 5) }
+        };
+        
+        context.Doadores.AddRange(doadoresIniciais);
+
+        foreach (var doadorNovo in doadoresIniciais)
+    {
+        var doadorExistente = context.Doadores.Find(doadorNovo.Id);
+        if (doadorExistente is not null)
+        {
+            doadorExistente.Nome = doadorNovo.Nome;
+            doadorExistente.TipoSanguineo = doadorNovo.TipoSanguineo;
+            doadorExistente.DataUltimaDoacao = doadorNovo.DataUltimaDoacao;
+        }
+        else
+        {
+            context.Doadores.Add(doadorNovo);
+        }
+    }
+
+        context.SaveChanges();
+    }
+}
